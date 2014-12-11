d5216a26f75baf2f08168ab8e6097a3bc1d38d3a
Gui Seek <guiseek@gmail.com>
2014-12-11 01:30:07 -0200

    crud complete

--- modules/user/controller.js
+++ modules/user/controller.js
@@ -7,16 +7,16 @@ app.controller('UserCtrl',['$rootScope','UserModel', function($rootScope,UserMod
   //var userFindAllError = $rootScope.$on('user:find_all_error', function(event, data) { findAllError(data); });
 
   // Event Listener
-  $rootScope.$on('user:find_all_success', function(event, data) { findAllSuccess(data);  });
-  $rootScope.$on('user:find_all_error', function(event, data) { findAllError(data); });
-  $rootScope.$on('user:find_success', function(event, data) { findSuccess(data);  });
-  $rootScope.$on('user:find_error', function(event, data) { findError(data); });
-  $rootScope.$on('user:create_success', function(event, data) { createSuccess(data);  });
-  $rootScope.$on('user:create_error', function(event, data) { createError(data); });
-  $rootScope.$on('user:update_success', function(event, data) { updateSuccess(data);  });
-  $rootScope.$on('user:update_error', function(event, data) { updateError(data); });
-  $rootScope.$on('user:remove_success', function(event, data) { updateSuccess(data);  });
-  $rootScope.$on('user:remove_error', function(event, data) { updateError(data); });
+  $rootScope.$on('user:find_all_success', function(event, data) { this._findAllSuccess(data);  });
+  $rootScope.$on('user:find_all_error', function(event, data) { this._findAllError(data); });
+  $rootScope.$on('user:find_success', function(event, data) { this._findSuccess(data);  });
+  $rootScope.$on('user:find_error', function(event, data) { this._findError(data); });
+  $rootScope.$on('user:create_success', function(event, data) { this._createSuccess(data);  });
+  $rootScope.$on('user:create_error', function(event, data) { this._createError(data); });
+  $rootScope.$on('user:update_success', function(event, data) { this._updateSuccess(data);  });
+  $rootScope.$on('user:update_error', function(event, data) { this._updateError(data); });
+  $rootScope.$on('user:remove_success', function(event, data) { this._updateSuccess(data);  });
+  $rootScope.$on('user:remove_error', function(event, data) { this._updateError(data); });
 
   var find = function(id) {
     return UserModel.find(id);
@@ -47,34 +47,34 @@ app.controller('UserCtrl',['$rootScope','UserModel', function($rootScope,UserMod
   }
 
   // Event Dispatcher
-  var findAllSuccess = function(data) {
+  _findAllSuccess = function(data) {
     self.users = data;
   }
-  var findAllError = function(data) {
+  _findAllError = function(data) {
     console.warn(data);
   }
-  var findSuccess = function(data) {
+  _findSuccess = function(data) {
     self.user = data;
   }
-  var findError = function(data) {
+  _findError = function(data) {
     console.warn(data);
   }
-  var createSuccess = function(data) {
+  _createSuccess = function(data) {
     self.users.push(data);
   }
-  var createError = function(data) {
+  _createError = function(data) {
     console.warn(data);
   }
-  var updateSuccess = function(data) {
+  _updateSuccess = function(data) {
     UserModel.findAll();
   }
-  var updateError = function(data) {
+  _updateError = function(data) {
     console.warn(data);
   }
-  var removeSuccess = function(data) {
+  _removeSuccess = function(data) {
     UserModel.findAll();
   }
-  var removeError = function(data) {
+  _removeError = function(data) {
     console.warn(data);
   }
 
